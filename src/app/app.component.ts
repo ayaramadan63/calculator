5cde5a0793f86181bf1feca41d05e6a384b67a4d
ayaramadan63 <aya.ramadan1717@gmail.com>
2022-10-30 17:04:08 +0200

    commit2

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'calculator';
+  result = '';
+
+
+
+  getAction(item: any) { 
+    this.result += item; 
+    
+  }
+  equal() {
+    
+    this.result = eval(this.result).toFixed(1); 
+
+  }
+
+  reset() {
+
+    this.result = '';
+  }
+
+}
